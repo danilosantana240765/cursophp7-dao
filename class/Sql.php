defd31f4febafa2dc82b0820b819d5781ded9740
danilosantana240765 <38994152+danilosantana240765@users.noreply.github.com>
2021-02-06 20:38:22 -0300

    Iniciamos o projeto

--- class/Sql.php
+++ class/Sql.php
@@ -1,34 +1,30 @@
 <?php
-class Sql extends PDO{
+
+class Sql extends PDO {
 	private $conn;
 
-	public function __construct(){
-		$this->conn = new PDO("mysql: host=localhost; dbname=dbphp7", "root", "");
+	public function __construct() {
+		$this->conn = new PDO("mysql:host=localhost;dbname=chat", "root", "");
 	}
 
-	private function setParams($statment, $parameters = array()){
-		foreach($parameters as $key => $value){
+	private function setParams($statment, $parameters = []) {
+		foreach ($parameters as $key => $value) {
 			$this->setParam($statment, $key, $value);
 		}
 	}
 
-	private function setParam($statment, $key, $value){
+	private function setParam($statment, $key, $value) {
 		$statment->bindParam($key, $value);
 	}
 
-	public function query($rawQuery, $params = array()){
-
+	public function query($rawQuery, $params = array()) {
 		$stmt = $this->conn->prepare($rawQuery);
-
 		$this->setParams($stmt, $params);
-
 		$stmt->execute();
 		return $stmt;
 	}
 
-	public function select($rawQuery, $params = array()):array{
-		$stmt = $this->query($rawQuery, $params);
-
-		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	public function select($rawQuery, $parameters = array()):array {
+		return $this->query($rawQuery, $parameters)->fetchAll(PDO::FETCH_ASSOC);
 	}
-}
+} 
--- index3.php
+++ index3.php
@@ -1,38 +1,8 @@
 <?php
-require_once("config.php");
+require_once "config.php";
 
+$sql = new Sql();
 
-//Carrega um usuario
-/*$root = new Usuario();
-$root->loadById(4);
-echo $root*/
-// Carrega lista de usuarios
-//$lista = Usuario::getList();
+$usuarios = $sql->select("SELECT * FROM meu_chat");
 
-//echo json_encode($lista);
-
-//Carrega uma lista de usuario buscando por login
-
-//$search = Usuario::search("us");
-//echo json_encode($search);
-
-//Carrega o usuario o login e uma senha
-
-// Criando um novo usuario
-//$usuario = new Usuario();
-//$usuario->login("danilo", "12345678d");
-/*$aluno = new Usuario("hcode", "12345");
-$aluno->insert();
-echo $aluno;*/
-// Alterar um usuarios
-/*$usuario = new Usuario();
-$usuario->loadById(10);
-$usuario->update("professor", "professor");
-
-echo $usuario;*/
-
-$usuario = new Usuario();
-$usuario->loadById(11);
-$usuario->delete();
-
-echo $usuario;
+echo json_encode($usuarios);
